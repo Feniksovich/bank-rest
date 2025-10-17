e50b70f2a8835be3c4074e1767aaab099935b6e0
Feniksovich <maxsimus.i@yandex.ru>
2025-10-17 15:44:13 +0600

    feat: security configurer

---
+++ src/main/java/com/feniksovich/bankcards/config/SecurityConfigurer.java
@@ -0,0 +1,87 @@
+package com.feniksovich.bankcards.config;
+
+import com.feniksovich.bankcards.entity.Role;
+import com.feniksovich.bankcards.security.BearerJwtAuthenticationTokenConverter;
+import com.feniksovich.bankcards.security.ExtendedUserDetailsService;
+import com.feniksovich.bankcards.security.JwtAuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.servlet.util.matcher.PathPatternRequestMatcher;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfigurer {
+
+    private final ExtendedUserDetailsService userDetailsService;
+
+    private static final int PASSWORD_ENCODER_BCRYPT_ROUNDS = 12;
+
+    @Autowired
+    public SecurityConfigurer(ExtendedUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(
+            HttpSecurity httpSecurity,
+            AuthenticationManager authenticationManager,
+            BearerJwtAuthenticationTokenConverter bearerJwtConverter,
+            CorsConfigurationSource corsConfigurationSource
+    ) throws Exception {
+        final AuthenticationFilter jwtAuthenticationFilter = new AuthenticationFilter(authenticationManager, bearerJwtConverter);
+        jwtAuthenticationFilter.setRequestMatcher(PathPatternRequestMatcher.withDefaults().matcher("/**"));
+        jwtAuthenticationFilter.setSuccessHandler((_, _, _) -> {});
+        return httpSecurity
+                .cors(configurer -> configurer.configurationSource(corsConfigurationSource))
+                .sessionManagement(configurer ->
+                        configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .exceptionHandling(configurer ->
+                        configurer.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/account/**").hasRole(Role.USER.name());
+                    registry.requestMatchers("/cards/**").hasRole(Role.ADMIN.name());
+                    registry.requestMatchers("/users/**").hasRole(Role.ADMIN.name());
+                    registry.anyRequest().permitAll();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(PASSWORD_ENCODER_BCRYPT_ROUNDS);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            PasswordEncoder passwordEncoder,
+            JwtAuthenticationProvider jwtAuthenticationProvider
+    ) {
+        // Username and password authentication provider
+        final DaoAuthenticationProvider daoAuthenticationProvider =
+                new DaoAuthenticationProvider(userDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        // Register along with JWT authentication provider
+        return new ProviderManager(daoAuthenticationProvider, jwtAuthenticationProvider);
+    }
+}
