c1c681495966d709b1411d8faecd3bae44d0d9c4
feniksovich <maxsimus.i@yandex.ru>
2025-10-18 14:44:06 +0600

    feat: add custom card expiration date serializer

--- src/main/java/com/feniksovich/bankcards/dto/card/CardData.java
+++ src/main/java/com/feniksovich/bankcards/dto/card/CardData.java
@@ -1,5 +1,6 @@
 package com.feniksovich.bankcards.dto.card;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,7 +17,10 @@ import java.util.UUID;
 public class CardData {
     private UUID id;
     private String panLast4;
+
+    @JsonSerialize(using = CardExpirationDateSerializer.class)
     private LocalDate expiresAt;
+
     private String cardHolder;
     private BigDecimal balance;
     private boolean blocked;
---
+++ src/main/java/com/feniksovich/bankcards/dto/card/CardExpirationDateSerializer.java
@@ -0,0 +1,23 @@
+package com.feniksovich.bankcards.dto.card;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class CardExpirationDateSerializer extends JsonSerializer<LocalDate> {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("MM/yy");
+
+    @Override
+    public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        if (value == null) {
+            gen.writeNull();
+        } else {
+            gen.writeString(value.format(FORMATTER));
+        }
+    }
+}
